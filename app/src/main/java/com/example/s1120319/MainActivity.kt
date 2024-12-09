3de02b8733cc6acea0282b15abef9b56e7f54550
Eva_025 <94.05.25jj@gmail.com>
2024-12-09 14:34:16 +0800

    v1_張綺庭

--- app/src/main/java/com/example/s1120319/MainActivity.kt
+++ app/src/main/java/com/example/s1120319/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.s1120319
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -20,6 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             S1120319Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+
+
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
